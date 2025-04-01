931ed661014d42a79fdca86951a8480dd6b17366
kefirchk <prostolex2004@mail.ru>
2025-04-01 03:47:22 +0300

    Linked FE auth with BE auth

--- app/src/views/auth.py
+++ app/src/views/auth.py
@@ -1,19 +1,15 @@
 import streamlit as st
-from utils.resource_loader import ResourceLoader
+from services.auth import AuthService
+from services.resource import ResourceService
 
-st.set_page_config(page_title="Animatica - Auth", layout="centered")
+# st.set_page_config(page_title="Animatica - Auth", layout="centered")
 
 # Load styles and templates
-ResourceLoader.load_styles("auth.css")
-main_container_template = ResourceLoader.load_template("auth/main_container.html")
-auth_header_template = ResourceLoader.load_template("auth/header.html")
-login_column_template = ResourceLoader.load_template("auth/login_column.html")
-register_column_template = ResourceLoader.load_template("auth/register_column.html")
-
-
-# TODO: Delete after adding Database
-if "USER_CREDENTIALS" not in st.session_state:
-    st.session_state.USER_CREDENTIALS = {"user1": "password1", "user2": "password2"}
+ResourceService.load_styles("auth.css")
+main_container_template = ResourceService.load_template("auth/main_container.html")
+auth_header_template = ResourceService.load_template("auth/header.html")
+login_column_template = ResourceService.load_template("auth/login_column.html")
+register_column_template = ResourceService.load_template("auth/register_column.html")
 
 
 # Message States
@@ -25,35 +21,8 @@ if "register_success" not in st.session_state:
     st.session_state.register_success = ""
 
 
-# Login Function
-def login(username: str, password: str) -> bool:
-    if username in st.session_state.USER_CREDENTIALS:
-        if st.session_state.USER_CREDENTIALS[username] == password:
-            st.session_state.logged_in = True
-            st.session_state.username = username
-            st.session_state.login_error = ""
-            return True
-    st.session_state.login_error = "Invalid username or password"
-    return False
-
-
-# Register Function
-def register(username: str, password: str, confirm_password: str) -> bool:
-    if username in st.session_state.USER_CREDENTIALS:
-        st.session_state.register_error = "Username already exists"
-        return False
-    if password != confirm_password:
-        st.session_state.register_error = "Passwords don't match"
-        return False
-
-    st.session_state.USER_CREDENTIALS[username] = password
-    st.session_state.register_error = ""
-    st.session_state.register_success = "Registration successful! Please login."
-    return True
-
-
 # Main Interface
-if "logged_in" in st.session_state and st.session_state.logged_in:
+if st.session_state.get("logged_in", False):
     st.success(f"Welcome back, {st.session_state.username}!")
     st.rerun()
 else:
@@ -71,7 +40,7 @@ else:
             password = st.text_input("Password", type="password", key="login_password")
 
             if st.form_submit_button("Login", type="primary"):
-                if login(st.session_state.login_username, st.session_state.login_password):
+                if AuthService.login(st.session_state.login_username, st.session_state.login_password):
                     st.rerun()
 
             if st.session_state.login_error:
@@ -88,7 +57,7 @@ else:
             confirm_password = st.text_input("Confirm Password", type="password", key="reg_confirm_password")
 
             if st.form_submit_button("Register", type="primary"):
-                if register(
+                if AuthService.register(
                     st.session_state.reg_username, st.session_state.reg_password, st.session_state.reg_confirm_password
                 ):
                     st.rerun()
